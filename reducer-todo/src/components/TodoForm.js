690cff1aefd5921d006fa2ebbc3b2c0227c39ce9
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-10 14:52:59 -0500

    adding form file

---
+++ reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React, { useState, useReducer } from 'react'
+import { initialState, todoReducer } from "../reducers/Reducers";
+
+const TodoList = props => {
+    const [state1,dispatch] = useReducer(todoReducer,initialState);
+    const [newTitle, setNewTitle] = useState('');
+
+const handleChanges = e => {
+    setNewTitle(e.target.value);
+}
+console.log('reducer state', state1)
+
+return (
+    <div>
+        <h1>REducer Todo</h1>
+
+        <input
+        className='Input'
+        type='text'
+        name='newTitle'
+        value={newTitle}
+        onChange={handleChanges}
+        />
+        
+
+
+
+
+
+
+    </div>
+)
+
+
+}
